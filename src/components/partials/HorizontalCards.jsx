65d9f32034f1b99602c51c9bd004a5297138fbb8
Preet Karwal <preetverma365@gmail.com>
2025-03-25 11:26:00 +0530

    Horizontal cards  ui update

--- src/components/partials/HorizontalCards.jsx
+++ src/components/partials/HorizontalCards.jsx
@@ -1,40 +1,47 @@
 import { Link } from "react-router-dom";
-import noimage from '/No_Image.png'
-
+import noimage from '/No_Image.png';
 
 const HorizontalCards = ({ data }) => {
-  // console.log("h card data" , data);
   return (
-    <div className="w-[100%] flex overflow-y-hidden mb-5 p-5 ">
-      {data.length > 0 ? data.map((d, i) => (
-        <Link to={`/${d.media_type}/details/${d.id}`} key={i} className="min-w-[15%] h-[35vh] bg-zinc-800 m-5 mb-5 rounded-lg">
-          <img
-            className="w-full h-[55%] object-cover"
-            src={d.backdrop_path || d.profile_path || d.poster_path ? `https://image.tmdb.org/t/p/original/${
-              d.backdrop_path || d.profile_path || d.poster_path
-            }`: noimage}
-            alt=""
-          />
+    <div className="w-full overflow-x-auto  mb-5 p-5 flex gap-5">
+      {data.length > 0 ? (
+        data.map((d, i) => (
+          <Link
+            to={`/${d.media_type}/details/${d.id}`}
+            key={i}
+            className="inline-block w-[70vw] sm:w-[45vw] md:w-[30vw] lg:w-[20vw] h-[35vh] bg-zinc-800 rounded-lg flex-shrink-0 overflow-hidden"
+          >
+            <img
+              className="w-full h-[55%] object-cover rounded-t-lg"
+              src={
+                d.backdrop_path || d.profile_path || d.poster_path
+                  ? `https://image.tmdb.org/t/p/original/${
+                      d.backdrop_path || d.profile_path || d.poster_path
+                    }`
+                  : noimage
+              }
+              alt=""
+            />
 
-          <div className="text-white p-3 h-[45%] overflow-y-auto">
-            <h1 className="text-xl font-semibold">
-              {d.original_title || d.name || d.original_name || d.title}:
-            </h1>
+            <div className="text-white p-3 h-[45%] flex flex-col justify-start gap-2">
+              <h1 className="text-xl font-semibold truncate">
+                {d.original_title || d.name || d.original_name || d.title}
+              </h1>
 
-            {/* <p className="w-[70%] text-white">{data.overview.slice(0,200)}</p> */}
-            {d.overview && (
-              <p className="">
-                {d.overview.slice(0, 50)}...
-                <span className="text-orange-100">more</span>
-              </p>
-            )}
-          </div>
-        </Link>
-      )) : (
-        <h1 className="text-white mt-5 text-3xl font-black text-center ">Nothing to show</h1>
-      )
-
-    }
+              {d.overview && (
+                <p className="text-sm sm:text-base md:text-lg leading-relaxed break-words">
+                  {d.overview.slice(0, 100)}...
+                  <span className="text-orange-100">more</span>
+                </p>
+              )}
+            </div>
+          </Link>
+        ))
+      ) : (
+        <h1 className="text-white mt-5 text-3xl font-black text-center">
+          Nothing to show
+        </h1>
+      )}
     </div>
   );
 };
